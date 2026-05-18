a55428a352fa0c553649861d099537c385fe311a
Keroles Mezare <keroles.mezare2005@gmail.com>
2026-05-18 17:05:04 +0300

    End of section 2

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,31 @@
+using System.Formats.Asn1;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MembersController(AppDbContext context) : ControllerBase
+{
+    
+
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        return await context.Users.ToListAsync();
+    }
+
+
+    [HttpGet("{Id}")]
+    public async Task<ActionResult<AppUser>> GetMember(string id )
+    {
+        var member = await context.Users.FindAsync(id); 
+        if(member == null) return NotFound();
+
+        return member;
+    }
+
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+namespace API.Entities;
+
+public class AppUser
+{
+    public string Id {set;get;} = Guid.NewGuid().ToString();    
+    public required string DisplayName{ set ;    get;}
+    public required string Email{get;set;}
+}
